ac14457a43bdaa883b15657a1569687395798e3e
nityulam <nityulam@mail.ru>
2025-01-10 15:52:27 +0700

    add get-report-JSON.js

---
+++ src/formator/get-report-JSON.js
@@ -0,0 +1,28 @@
+const formatter = (data) => {
+  // console.log('-<->- data', typeof data);
+  const result = data.map((node) => {
+    // console.log('node', node);
+    // console.log(node.status);
+    if (node.status === 'deleted') {
+      return `  - ${node.key}: ${node.value}`;
+    }
+    if (node.status === 'added') {
+      return `  + ${node.key}: ${node.value}`;
+    }
+    if (node.status === 'unchanged') {
+      return `    ${node.key}: ${node.value}`;
+    }
+    if (node.status === 'changed') {
+      return `  - ${node.key}: ${node.value}\n  + ${node.key}: ${node.value2}`;
+    }
+    return `    ${node.key}: ${node.value}`;
+  });
+  // const result = JSON.stringify(data, null, 2);
+  // switch (`${}`)
+
+  const tmp = ['{', ...result, '}'].join('\n');
+
+  return tmp;
+};
+
+export { formatter };
