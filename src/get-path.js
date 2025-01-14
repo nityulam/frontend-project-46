bad7c233e3ed1583f05f8f84f3dddbe8bd3ba35f
nityulam <nityulam@mail.ru>
2025-01-14 11:43:14 +0700

    fix get-path.js

--- src/get-path.js
+++ src/get-path.js
@@ -2,4 +2,4 @@ import path from 'node:path';
 // Формируем полный путь для запуска
 const getFullPath = (pathFile) => path.resolve(process.cwd(), pathFile);
 
-export { getFullPath };
+export default getFullPath;
