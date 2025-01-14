9fa3a31debfa6558cb923fa09270538136b6a236
nityulam <nityulam@mail.ru>
2025-01-14 11:41:46 +0700

    fix get-date.js

--- src/get-date.js
+++ src/get-date.js
@@ -2,4 +2,4 @@ import { readFileSync } from 'node:fs';
 // Читаем файл (получем данные из файла)
 const getDate = (file) => readFileSync(file, 'utf-8');
 
-export { getDate };
+export default getDate;
