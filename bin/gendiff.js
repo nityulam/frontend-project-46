7096d62764a36ec7a808c04fb6abb624662ed901
nityulam <nityulam@mail.ru>
2025-01-24 15:56:13 +0700

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import getDiffFiles from '../src/index.js';
+import genDiff from '../src/index.js';
 
 program
   .version('1.0.0', '-V, --version', 'output the version number')
@@ -9,8 +9,8 @@ program
   .option('-f, --format [type]', 'output format (default: "stylish")')
   .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(getDiffFiles(filepath1, filepath2));
+  .action((filepath1, filepath2, option) => {
+    console.log(genDiff(filepath1, filepath2, option.format));
   });
 
 program.parse(process.argv);
