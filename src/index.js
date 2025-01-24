d7f688dd6bce10c9b0cce900db6ca83f80863906
nityulam <nityulam@mail.ru>
2025-01-24 15:57:44 +0700

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import { extname } from 'node:path';
 import getDate from './get-date.js';
-// import { getFormat } from './get-file-extension.js';
+
 import getFullPath from './get-path.js';
 import getParseDate from './get-parse-date.js';
 
-// import { jsonDiff } from './get-differences.js';
 import getTreeDifferences from './get-differences.js';
-import formatter from './formator/get-report-JSON.js';
+import formatter from './formatter/get-report-JSON.js';
+// import formatter from './formatter/stylish.js';
 
-// import { tmp } from './formator/tmp.js';
-
-const getDiffFiles = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
   // getDate Читаем файл (получем данные из файла), getFullPath Формируем полный путь для запуска
   const file1 = getDate(getFullPath(filepath1));
   const file2 = getDate(getFullPath(filepath2));
@@ -21,22 +19,21 @@ const getDiffFiles = (filepath1, filepath2) => {
   // const formatFile2 = extname(filepath2);
 
   // Получаем распарсенные данные в зависимости от формата(extname - получаем расширение файла)
-  const fileDate1 = getParseDate(file1, extname(filepath1));
-  const fileDate2 = getParseDate(file2, extname(filepath2));
+  const date1 = getParseDate(file1, extname(filepath1));
+  const date2 = getParseDate(file2, extname(filepath2));
   // console.log('- fileDate1 -', fileDate1);
   // console.log('- fileDate2 -', fileDate2);
 
-  // получаем отличия в виде массива со статусами
-  const differences = getTreeDifferences(fileDate1, fileDate2);
+  // получаем отличия в виде дерева (массива) со статусами
+  const differences = getTreeDifferences(date1, date2);
   // console.log('- отличия -', differences);
+  // console.log('- формат -', format);
 
   // Формируем итоговый отчет в указанном формате
-  const result = formatter(differences);
+  const result = formatter(differences, format);
   // console.log('-!- result -', result);
 
   return result;
-
-  // должен вернуть строку установленного формата JSON, yaml, yml
 };
 
-export default getDiffFiles;
+export default genDiff;
