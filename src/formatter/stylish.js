7540eb70a323072cb753dc26dd3b7d9db071f5fa
nityulam <nityulam@mail.ru>
2025-01-28 16:49:03 +0700

    update stylish.js

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -1,7 +1,34 @@
-const stringify = (data, depth) => {
-  if (typeof data !== 'object' || data === null) {
+import _ from 'lodash';
+
+const stylish = (data) => {
+  if (_.isPlainObject(data)) {
     return `${data}`;
   }
+  const iter = (obj, depth) => {
+    const margins = 2;
+    const indent = '  '.repeat(depth * margins - 2);
+
+    const result = obj.map((item) => {
+      const { key, value, oldValue, newValue, children, status } = item;
+
+      switch (status) {
+        case 'added':
+          return `  ${indent}+ ${key}: ${value}`;
+        case 'deleted':
+          return `  ${indent}- ${key}: ${value}`;
+        case 'unchanged':
+          return `  ${indent}  ${key}: ${value}`;
+        case 'changed':
+          return `  ${indent}- ${key}: ${oldValue}\n  ${indent}+ ${key}: ${newValue}`;
+        case 'hasChild':
+          return `  ${indent}  ${key}: ${iter(children, depth + 1)}`;
+        default:
+          throw new Error(`Лох что ли${status}`);
+      }
+    });
+    return ['{', ...result, `${indent}`, '}'].join('\n');
+  };
+  return iter(data, 1);
 };
 
-export default stringify;
+export default stylish;
