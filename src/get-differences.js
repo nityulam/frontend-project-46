4fad52dadd6e89e1540c0460c69dcd0a02ad0352
nityulam <nityulam@mail.ru>
2025-01-28 16:47:40 +0700

    fix get-differences.js

--- src/get-differences.js
+++ src/get-differences.js
@@ -6,36 +6,35 @@ const getTreeDifferences = (data1, data2) => {
   const sortedUniqueKeys = _.sortBy(_.union(keys1, keys2));
 
   const result = sortedUniqueKeys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-
-    if (!Object.hasOwn(data1, key)) {
-      const result2 = { key, value: value2, status: 'added' };
-      // console.log('result', result2);
-      return result2;
-    }
-    if (!Object.hasOwn(data2, key)) {
-      const result3 = { key, value: value1, status: 'deleted' };
-      // console.log('result', result3);
-      return result3;
-    }
-    if (_.isEqual(value1, value2)) {
-      const result4 = { key, value: value1, status: 'unchanged' };
-      // console.log('result unchanged', result4);
-      return result4;
-    }
-
-    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      const child = getTreeDifferences(value1, value2);
+    // console.log('data1[key]', data1[key], 'key - ', key);
+    // console.log('data2[key]', data2[key], 'key - ', key);
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      const child = getTreeDifferences(data1[key], data2[key]);
       const result5 = { key, children: child, status: 'hasChild' };
       // console.log('result hasChild', result5);
       return result5;
     }
 
+    if (!Object.hasOwn(data1, key)) {
+      const result2 = { key, value: data2[key], status: 'added' };
+      // console.log('result', result2);
+      return result2;
+    }
+    if (!Object.hasOwn(data2, key)) {
+      const result3 = { key, value: data1[key], status: 'deleted' };
+      // console.log('result', result3);
+      return result3;
+    }
+    if (_.isEqual(data1[key], data2[key])) {
+      const result4 = { key, value: data1[key], status: 'unchanged' };
+      // console.log('result unchanged', result4);
+      return result4;
+    }
+
     const result6 = {
       key,
-      value: value1,
-      value2,
+      oldValue: data1[key],
+      newValue: data2[key],
       status: 'changed',
     };
     return result6;
