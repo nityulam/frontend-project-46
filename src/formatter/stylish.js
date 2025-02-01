6a55ee85f2f6bbd9b02778bd9ea9ed391ab06b07
nityulam <nityulam@mail.ru>
2025-02-01 17:06:47 +0700

    update stylish.js

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -1,34 +1,50 @@
 import _ from 'lodash';
 
+const indent = (depth) => {
+  const margin = '    ';
+  return margin.repeat(depth);
+};
+
+const stringify = (data, depth) => {
+  if (!_.isPlainObject(data)) {
+    return `${data}`;
+  }
+  const arrData = Object.entries(data);
+
+  const str = arrData.map(([key, value]) => {
+    const result = `  ${indent(depth)}  ${key}: ${stringify(value, depth + 1)}`;
+    // console.log('-key-', key);
+    return result;
+  });
+  return `{\n${str.join('\n')}\n${indent(depth)}}`;
+};
+
 const stylish = (data) => {
   if (_.isPlainObject(data)) {
     return `${data}`;
   }
   const iter = (obj, depth) => {
-    const margins = 2;
-    const indent = '  '.repeat(depth * margins - 2);
-
     const result = obj.map((item) => {
       const { key, value, oldValue, newValue, children, status } = item;
 
       switch (status) {
         case 'added':
-          return `  ${indent}+ ${key}: ${value}`;
+          return `  ${indent(depth)}+ ${key}: ${stringify(value, depth + 1)}`;
         case 'deleted':
-          return `  ${indent}- ${key}: ${value}`;
+          return `  ${indent(depth)}- ${key}: ${stringify(value, depth + 1)}`;
         case 'unchanged':
-          return `  ${indent}  ${key}: ${value}`;
+          return `  ${indent(depth)}  ${key}: ${stringify(value, depth + 1)}`;
         case 'changed':
-          return `  ${indent}- ${key}: ${oldValue}\n  ${indent}+ ${key}: ${newValue}`;
+          return `  ${indent(depth)}- ${key}: ${stringify(oldValue, depth + 1)}\n  ${indent(depth)}+ ${key}: ${stringify(newValue, depth + 1)}`;
         case 'hasChild':
-          return `  ${indent}  ${key}: ${iter(children, depth + 1)}`;
+          return `  ${indent(depth)}  ${key}: ${iter(children, depth + 1)}`;
         default:
-          throw new Error(`Лох что ли${status}`);
+          throw new Error(`Status ${status} is not supported`);
       }
     });
-    return ['{', ...result, `${indent}`, '}'].join('\n');
+    return `{\n${result.join('\n')}\n${indent(depth)}}`;
   };
-  return iter(data, 1);
+  return iter(data, 0);
 };
 
 export default stylish;
