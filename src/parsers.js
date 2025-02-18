e61b2cf457b9b1a4c3527ed3d964e9c0323d72c8
nityulam <nityulam@mail.ru>
2025-02-18 15:40:37 +0700

    fix parsers.js

---
+++ src/parsers.js
@@ -0,0 +1,15 @@
+import yaml from 'js-yaml';
+
+const getParseDate = (data, format) => {
+  switch (format) {
+    case '.json':
+      return JSON.parse(data);
+    case '.yaml':
+    case '.yml':
+      return yaml.load(data);
+    default:
+      throw new Error(`Extension ${format} it is not supported`);
+  }
+};
+
+export default getParseDate;
